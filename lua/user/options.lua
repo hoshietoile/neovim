4b5a0d7287928472a41449336bc829e142cfa3b3
hoshietoile <francekakumei714@gmail.com>
2022-12-24 05:19:34 +0000

    options

---
+++ lua/user/options.lua
@@ -0,0 +1,47 @@
+local o = vim.opt
+
+local options = {
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  timeoutlen = 1000,
+  undofile = true,
+  updatetime = 300,
+  writebackup = false,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  cursorline = true,
+  number = true,
+  relativenumber = false,
+  numberwidth = 4,
+  signcolumn = "yes",
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  guifont = "monospace:h17",
+}
+
+o.shortmess:append "c"
+
+for k, v in pairs(options) do
+  o[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]]
